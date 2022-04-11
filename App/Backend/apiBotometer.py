57a52b1dfdf7d2ea46fc29dd0a61848e413134a2
Pat Bar <59734015+Joberooo@users.noreply.github.com>
2022-04-11 09:40:42 +0200

    Add templates

---
+++ App/Backend/apiBotometer.py
@@ -0,0 +1,15 @@
+import botometer
+
+rapid_api_key = "a5c7976279mshc4c1e60955473b3p13e6d5jsn553f9c2f7cd8"
+twitter_app_auth = {
+    'consumer_key': 'WnMbrFKacyv0ITQyXtuKSjeUt',
+    'consumer_secret': 'RRvlf17vuvyaOKR1lm3DiUUg35m4AMlMfGR24t7CdZukFiGOIb',
+    'access_token': '1169122850726453249-nzt2nFIkB4kd2PMMUU6sLz3eOZVvHK',
+    'access_token_secret': '9nQ2TqBiAj7soc34ahcGyNekCfXDZ6WExWKJvuJ96Ct82',
+}
+boto_meter_object = botometer.Botometer(wait_on_ratelimit=True,
+                                        rapidapi_key=rapid_api_key,
+                                        **twitter_app_auth)
+
+# Check a single account by screen name
+result = boto_meter_object.check_account('@Zelazna_Dywizja')
--- App/app.py
+++ App/app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, flash
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def check_button_click():
+    print("ok")
+
+
+@app.route("/checkAccount")
+def check_account():
+    return render_template("checkAccount.html", content=None)
+
+
+@app.route("/")
+def main():
+    return render_template("index.html", content=None)
 
 
 if __name__ == '__main__':
